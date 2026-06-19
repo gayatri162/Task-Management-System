b38338f1f7204e3e39923b34c87879f153f6bfc7
Gayatri <gayatrisingh44567@gmail.com>
2026-06-19 13:09:12 +0530

    Enhance task management and user greeting
    Added confirmation prompt for clearing all tasks and implemented greeting based on the current hour. Enhanced task management features with priority and important flags.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks = [];
 let currentUser = "";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -8,8 +8,10 @@ window.addEventListener("DOMContentLoaded", () => {
  const clearBtn = document.getElementById("clearAllBtn");
  if (clearBtn) {
     clearBtn.addEventListener("click", function () {
-      tasks = [];
-      saveTasks();
+     if(confirm("Delete all tasks?")){
+       tasks = [];
+       saveTasks();
+}
     });
  }
 
@@ -20,6 +22,24 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+function setGreeting(){
+ const heading =
+ document.getElementById("greeting");
+ if(!heading) return;
+
+ const hour = new Date().getHours();
+ let msg = "Good Morning";
+
+ if(hour >= 12 && hour < 18){
+   msg = "Good Afternoon";
+ }
+ else if(hour >= 18){
+   msg = "Good Evening";
+ }
+ heading.innerText =
+ `${msg}, ${currentUser} 👋`;
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   const themeToggle = document.getElementById("themeToggle");
@@ -44,15 +64,38 @@ themeToggle.addEventListener("click", () => {
 });
 
 function signup(){
+ 
 const username = document.getElementById("username").value;
 const password = document.getElementById("password").value;
+ 
+if(!username || !password){
+  alert("Enter Username and Password");
+  return;
+}
 
 localStorage.setItem("username",username);
 localStorage.setItem("password",password);
 localStorage.setItem("lastUser", username);
+localStorage.setItem("isLoggedIn","true");
+
+
+currentUser = username;
+
+tasks =
+JSON.parse(
+localStorage.getItem("tasks_"+username)
+) || [];
 
 alert("Signup Successful");
+
+document.getElementById("auth-section").style.display="none";
+document.getElementById("app-section").style.display="grid";
+
+renderTasks();
+setGreeting();
 }
+
+
 
 function login(){
 
@@ -80,9 +123,10 @@ localStorage.getItem("tasks_"+username)
 
 document.getElementById("auth-section").style.display="none";
 
-document.getElementById("app-section").style.display="block";
+document.getElementById("app-section").style.display="grid";
 
 renderTasks();
+setGreeting();
 }
 else{
 
@@ -116,7 +160,8 @@ tasks.push({
  text: taskText,
  date: dueDate,
  completed: false,
- priority: priority
+ priority: priority,
+ important:false
 });
 
 saveTasks();
@@ -181,40 +226,62 @@ li.addEventListener("drop", (e) => {
   li.classList.add(task.priority);
   
  li.innerHTML=`
-   <div class="task-info">
+   
+<div class="task-row">
 
-    <span class="${task.completed ? "completed" : ""}">
-      ${task.text}
-    </span>
+<div class="left">
 
-    <small>Due: ${task.date || "No Date"}</small>
-    <small>Status: ${task.completed ? "Completed" : "Pending"}
-</small>
+<label class="check-container">
+<input
+type="checkbox"
+${task.completed ? "checked" : ""}
+onchange="toggleTask(${index})">
+</label>
 
 </div>
 
-<div class="task-buttons">
+<div class="center">
 
-<button onclick="toggleTask(${index})">
- ${task.completed ? "Undo" : "Done"}
+<span class="${task.completed ? "completed" : ""}">
+${task.text}
+</span>
+
+<small>
+📅 ${task.date || "No Date"}
+</small>
+
+<span class="badge ${task.priority}">
+${task.priority}
+</span>
+
+</div>
+
+<div class="right">
+
+<button
+class="important-btn ${task.important ? "active" : ""}"
+onclick="toggleImportant(${index})">
+${task.important ? "★" : "☆"}
 </button>
 
 <button onclick="editTask(${index})">
- Edit
+Edit
 </button>
 
 <button onclick="deleteTask(${index})">
- Delete
+Delete
 </button>
 
 </div>
-`;
+
+</div>`
 
 taskList.appendChild(li);
 
 });
 
 updateStats();
+updateUpcoming();
 }
 
 function toggleTask(index){
@@ -232,18 +299,63 @@ if(updatedTask){
 }
 
 function deleteTask(index){
+if(confirm("Delete this task?")){
  tasks.splice(index,1);
+ saveTasks();
+}
+}
+
+function toggleImportant(index){
+ tasks[index].important =
+ !tasks[index].important;
  saveTasks();
 }
 
 function updateStats(){
  document.getElementById("total").innerText = tasks.length;
+ 
+const completed =
+tasks.filter(task=>task.completed).length;
+
+const percent =
+tasks.length
+? (completed/tasks.length)*360
+: 0;
+
+document
+.querySelector(".circle")
+.style.setProperty(
+"--progress",
+`${percent}deg`
+);
+
+ document.getElementById("circleTotal").innerText = 
+ `${completed}/${tasks.length}`;
 
  document.getElementById("completed").innerText = 
  tasks.filter(task=>task.completed).length;
 
 document.getElementById("pending").innerText =
 tasks.filter(task=>!task.completed).length;
+
+document.getElementById(
+"allCount"
+).innerText =
+tasks.length;
+
+document.getElementById(
+"importantCount"
+).innerText =
+tasks.filter(
+t=>t.important
+).length;
+
+document.getElementById(
+"completedCount"
+).innerText =
+tasks.filter(
+t=>t.completed
+).length;
 }
 
 function searchTask(){
@@ -257,7 +369,7 @@ items.forEach(item=>{
  const text = item.innerText.toLowerCase();
 
 if(text.includes(input)){
- item.style.display="flex";
+ item.style.display="block";
 } else {
   item.style.display="none";
    }
@@ -265,7 +377,55 @@ if(text.includes(input)){
 }
 
 
-window.addEventListener() = function(){
+function updateUpcoming(){
+ const list =
+ document.getElementById("upcomingList");
+
+if(!list) return;
+
+const upcoming =
+ tasks
+.filter(
+task =>
+task.date &&
+!task.completed
+)
+ .sort(
+ (a,b)=>
+ new Date(a.date) -
+ new Date(b.date)
+ )
+ .slice(0,3);
+
+if(upcoming.length === 0){
+list.innerHTML =
+"<li>No upcoming tasks</li>";
+return;
+}
+
+ list.innerHTML = "";
+
+ upcoming.forEach(task=>{
+  list.innerHTML += `
+  <li>
+  <strong>${task.text}</strong>
+  <br>
+  <small>📅 ${task.date}</small>
+  <br>
+  <small>🔥 ${task.priority}</small> 
+  </li>
+  `;
+ });
+
+if(tasks.length === 0){
+ list.innerHTML = 
+ "<li>No upcoming tasks</li>";
+ return;
+}
+
+}
+
+window.addEventListener("load", function(){  
  if(localStorage.getItem("isLoggedIn")){
   const savedUser = localStorage.getItem("lastUser");
 
@@ -275,8 +435,106 @@ tasks =
  JSON.parse(localStorage.getItem("tasks_"+savedUser)) || [];
 
 document.getElementById("auth-section").style.display="none";
-document.getElementById("app-section").style.display="block";
+document.getElementById("app-section").style.display="grid";
 
 renderTasks();
+setGreeting();
 }
-};
+});
+
+document
+.getElementById("allTasks")
+.addEventListener(
+"click",
+()=>renderTasks()
+);
+
+document
+.getElementById("completedTasks")
+.addEventListener(
+"click",
+()=>filterTasks("completed")
+);
+
+document
+.getElementById("importantTasks")
+.addEventListener(
+"click",
+()=>filterTasks("high")
+);
+
+function filterTasks(type){
+
+const taskList =
+document.getElementById("taskList");
+
+taskList.innerHTML="";
+
+let filtered = [];
+
+if(type==="completed"){
+
+filtered =
+tasks.filter(
+task=>task.completed
+);
+
+}
+else if(type==="high"){
+
+filtered =
+tasks.filter(
+task=>task.important
+);
+
+}
+renderFiltered(filtered);
+}
+
+function renderFiltered(filtered){
+
+if(filtered.length === 0){
+taskList.innerHTML = `
+<p id="emptyMsg">
+No Important Tasks ⭐
+</p>
+`;
+return;
+}
+
+const taskList =
+document.getElementById("taskList");
+
+taskList.innerHTML = "";
+
+filtered.forEach((task,index)=>{
+
+const li = document.createElement("li");
+
+li.innerHTML = `
+<div class="task-row">
+
+<div class="center">
+
+<span class="${task.completed ? "completed" : ""}">
+${task.text}
+</span>
+
+<small>📅 ${task.date || "No Date"}
+</small>
+
+${task.important ? "<small>⭐ Important</small>" : ""}
+
+<span class="badge ${task.priority}">
+${task.priority}
+</span>
+</div>
+
+</div>
+`;
+
+taskList.appendChild(li);
+
+});
+
+}
